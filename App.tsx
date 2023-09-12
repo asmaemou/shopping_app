dbe314831fc108620125ce788db74039f18963dd
asmaemou <amouradi2002@gmail.com>
2023-09-12 21:06:47 +0100

    work on filter page
    work on filter page and the solution is to import { SafeAreaProvider } from 'react-native-safe-area-context'; and wrap the whole return() function with it

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { useMemo } from 'react';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const theme: Theme = useMemo(() => ({...DefaultTheme,colors:{
@@ -20,15 +21,17 @@ export default function App() {
 []
 );
   return (
-    <GestureHandlerRootView style={styles.container}>
+    <SafeAreaProvider> 
+      <GestureHandlerRootView style={styles.container}>
       <NavigationContainer theme={theme}>
         <BottomSheetModalProvider>
           <RootNavigator/>
-
         </BottomSheetModalProvider>
         <StatusBar style="dark"/>
       </NavigationContainer>
     </GestureHandlerRootView>
+    </SafeAreaProvider>
+    
   );
 }
 
