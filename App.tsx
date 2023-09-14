c61eafefcabb1bb764013a1ce966dd8822ed1bc4
asmaemou <amouradi2002@gmail.com>
2023-09-14 11:52:06 +0100

    login page
    adding the login page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import {DefaultTheme, NavigationContainer, Theme} from "@react-navigation/native";
 import RootNavigator from "./src/navigators/RootNavigator";
 import { useMemo } from 'react';
@@ -7,8 +7,21 @@ import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider, useAuth } from './app/context/AuthContext';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import { TabsStackScreenProps } from './src/navigators/TabsNavigator';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  return(
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
+  )
   const theme: Theme = useMemo(() => ({...DefaultTheme,colors:{
     ...DefaultTheme.colors,
     background:"#f5f5f5",
@@ -32,6 +45,27 @@ export default function App() {
     </GestureHandlerRootView>
     </SafeAreaProvider>
     
+  );
+}
+
+export const Layout = ({ navigation }: TabsStackScreenProps<"Home">) => {
+  const { authState, onLogout } = useAuth();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {authState?.authenticated ? (
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerRight: () => <Button onPress={onLogout} title="Sign out" />,
+            }}
+          />
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,78 @@
+import { Image, View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { useAuth } from '../../app/context/AuthContext';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { onLogin, onRegister } = useAuth();
+
+  const login = async () => {
+    const result = await onLogin!(email, password);
+    if (result && result.error) {
+      alert(result.msg);
+    }
+  };
+
+  // We automatically call the login after a successful registration
+  const register = async () => {
+    const result = await onRegister!(email, password);
+    if (result && result.error) {
+      alert(result.msg);
+    } else {
+      login();
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text: string) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text: string) => setPassword(text)}
+          value={password}
+        />
+        <Button title="Login" onPress={login} />
+        <Button title="Register" onPress={register} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  form: {
+    width: '80%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+});
+
+export default LoginScreen;
